81cb896442df932ba6830a7cd1363fcac1cd0206
bungaepyo <bungaepyo@gmail.com>
2021-03-09 20:15:13 -0500

    mutations

--- graphql/db.js
+++ graphql/db.js
@@ -1,25 +1,49 @@
-export const people = [
+let movies = [
+    {
+        id: 0,
+        name: "Star Wars",
+        score: 4
+    },
     {
         id: 1,
-        name: "toby",
-        age: 23,
-        gender: "male"
+        name: "Avengers",
+        score: 4
     },
     {
         id: 2,
-        name: "toby",
-        age: 23,
-        gender: "male"
+        name: "The Godfather",
+        score: 5
     },
     {
         id: 3,
-        name: "toby",
-        age: 23,
-        gender: "male"
-    },
+        name: "Logan",
+        score: 3
+    }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-    const filteredPeople = people.filter(person => id === person.id);
-    return filteredPeople[0];
+    const filteredMovies = movies.filter(movie => id === movie.id);
+    return filteredMovies[0];
+}
+
+export const deleteMovie = (id) => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
+    if(movies.length > cleanedMovies.length) {
+        movies = cleanedMovies;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: `${movies.length + 1}`,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
 }
